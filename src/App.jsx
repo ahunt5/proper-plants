9c7272b9a761be47f0e6d415b60b9c1b8c3153d2
Albert Hunt <albert.hunt10@gmail.com>
2026-02-04 14:48:00 -0600

    adds plant grid and styling with add to cart button

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+import PLANTS from "./data";
+import PlantList from "./plants/PlantList";
+import "./plants/plants.css";
+// pushing to array doesnt change the memory address, so react doesnt rerender
+// react rerenders if memory address changes
+// .push .pop doesnt work
+// treat React state is immutable, don't directly change it
+// setCart on a copy of the array
+// copy has a new memory address
+// const cartCopy = [...cart, item];
+const item = { image: "image", id: "1", name: "test", quantity: 1 };
 export default function App() {
-  return <></>;
+  const [cart] = useState(PLANTS);
+  return (
+    <>
+      <header>
+        <h1>Proper Plants</h1>
+      </header>
+      <main>
+        <PlantList plants={PLANTS}></PlantList>
+      </main>
+    </>
+  );
 }
---
+++ src/plants/PlantList.jsx
@@ -0,0 +1,20 @@
+// get list of plants
+// destructure list of plants
+// get the names, ids, and image of each plant
+// put an image of the plant, then an h3 of the plant name
+// add a btn below the plant name
+// copy an array or object with the spread operator (...)
+import PLANTS from "../data";
+import Plant from "./plant";
+export default function PlantList({ plants, addToCart }) {
+  return (
+    <section className="plants">
+      <h2>Plants</h2>
+      <ul>
+        {plants.map((plant) => (
+          <Plant key={plant.id} plant={plant} addToCart={addToCart}></Plant>
+        ))}
+      </ul>
+    </section>
+  );
+}
